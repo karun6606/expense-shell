38b361874b215f672004dcf26f55421ea64eada7
karun6606 <karun6606@gmail.com>
2024-07-14 16:41:43 +0530

    expenseshell.sh

--- backend.sh
+++ backend.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | awk -F "." '{print $1F}')
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
 if [ $USERID -ne 0 ]
 then    
@@ -18,50 +21,56 @@ else
     echo "$2..... Sucess"
 fi
 }
-dnf module disable nodejs -y
+dnf module disable nodejs -y &>>$LOGFILE
 VALIDATE $? "Disabled nodejs"
 
-dnf module enable nodejs:20 -y
+dnf module enable nodejs:20 -y &>>$LOGFILE
 VALIDATE $? "enable nodejs:20"
 
-dnf install nodejs -y
+dnf install nodejs -y &>>$LOGFILE
 VALIDATE $? "Installed nodejs"
 
-useradd expense
+#useradd expense
 
-# id expense
-# if [ $? -ne 0 ]
-# then
-#     useradd expense
-# else
-#     echo "user already exits..... SKIPPING"
-# fi
+id expense
+if [ $? -ne 0 ]
+then
+    useradd expense
+else
+    echo "user already exits..... SKIPPING"
+fi
 
-# mkdir /app
+mkdir /aapp
 
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip
 
-# curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip
+cd /app
 
-# cd /app
+#rm -rf /tmp/
 
-# unzip /tmp/backend.zip
+unzip /tmp/backend.zip
 
-# npm install
+npm install
 
-# cp 
-# C:\devops\daws-78s1\repos\expense-shell\backend.service /etc/systemd/system/backend.service
+ cp 
+C:\devops\daws-78s1\repos\expense-shell\backend.service /etc/systemd/system/backend.service
 
-# systemctl daemon-reload
+systemctl daemon-reload &>>$LOGFILE
+VALIDATE $? "daemon reload"
 
-# systemctl start backend
+systemctl start backend &>>$LOGFILE
+VALIDATE $? "starting backend"
 
-# systemctl enable backend
+systemctl enable backend &>>$LOGFILE
+VALIDATE $? "enabling backend"
 
-# dnf install mysql -y
+dnf install mysql -y &>>$LOGFILE
+VALIDATE $? "Installing mysql client"
 
-# mysql -h db.hornet78s.online -uroot -pExpenseApp@1 < /app/schema/backend.sql
+mysql -h db.hornet78s.online -uroot -pExpenseApp@1 < /app/schema/backend.sql
 
-# systemctl restart backend
+systemctl restart backend &>>$LOGFILE
+VALIDATE $? "Restarting backend"
 
 
 
